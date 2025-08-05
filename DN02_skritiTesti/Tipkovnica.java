d370ce1f598e1c7ab0ddef78aa9ddbd800e7ba3f
Logar05 <logarsamuel1@gmail.com>
2025-08-05 18:07:16 +0200

    koncana spirala

--- DN02_skritiTesti/Tipkovnica.java
+++ DN02_skritiTesti/Tipkovnica.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Tipkovnica {
     public static void main(String[] args) {
@@ -60,8 +61,78 @@ public class Tipkovnica {
         return travel;
     }
     public static int spiralnica(int d, int n, Scanner sc) {
+        int travel = 0;
+        int prva = sc.nextInt();
+        int prvaLayer = findLayer(prva, d);
+        int prvaCoord[] = findCoords(d, prva, prvaLayer);
+        for(int i = 1; i < n; i++) {
+            int next = sc.nextInt();
+            int nextLayer  = findLayer(next, d);
+            int nextCoords[] = findCoords(d, next, nextLayer);
+            travel += Math.abs(prvaCoord[0] - nextCoords[0]) + Math.abs(prvaCoord[1] - nextCoords[1]);
+            prvaCoord = nextCoords;
+            prvaLayer = nextLayer;
+            prva = next;
+        }
 
+        return travel;
+    }
+    public static int findLayer(int n, int d) {
+        if(n == 0) return 0;
+        int layer = 1;
+        for(int i = 1; i < d; i+=2){
+            if(i * i <= n && n < (i + 2) * (i + 2)){
+                return layer; 
+            }
+            layer++;
+        }
+        return layer;
+    }
+    public static int[] findCoords(int d, int n, int layer) {
+        int mid = d / 2 + 1;
+        if(n == 0) return new int[] {mid, mid};
+        int coords[] = new int[2];
+        int start = (int)Math.sqrt(n);
+        if(start % 2 == 0) start--;
+        start = start * start;
+        int layerLen = layer * 2;
+        int maxLayer = findLayer((d) * (d) - 1, d);
 
-        return 0;
+        int col, row;
+        int side = 0;
+        int start2 = start;
+        for(int i = 1; i <= 4; i++) {
+            if(n >= start && n < start + layerLen) {
+                side = i;
+                break;
+            }
+            start = start + layerLen;
+        }
+        if(side == 1 || n == start2 + layerLen) {
+            coords[0] = mid - layer;
+            coords[1] = maxLayer - layer + 1 + (n - start2);
+        }
+        if(side == 3 || n == start2 + 3 * layerLen){
+            coords[0] = mid + layer;
+            int botLeft = start2 + 3 * layerLen;
+            coords[1] = maxLayer - layer + 1 + botLeft - n;
+        }
+        if(side == 2 || n == start2 + 2 * layerLen){
+            int tmp = start2 + layerLen;
+            coords[0] = n - tmp + (maxLayer - layer) + 1;
+            
+            coords[1] = mid + layer;
+        }
+        if(side == 4) {
+            int tmp = start2 + 3 * layerLen;
+            int diff = n - tmp;
+            int corner = mid + layer;
+            coords[0] = corner - diff;
+            coords[1] = mid - layer;
+        }
+
+        return coords;
     }
 }
+
+
