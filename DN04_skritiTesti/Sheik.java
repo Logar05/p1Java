fbb6e46c3ff15977d1549e44d0f744593cc70b7d
Logar05 <logarsamuel1@gmail.com>
2025-08-06 17:41:58 +0200

    DN04 removed magic numbers

--- DN04_skritiTesti/Sheik.java
+++ DN04_skritiTesti/Sheik.java
@@ -1,22 +1,21 @@
 import java.util.*;
 public class Sheik {
+    public static final int MAX_SIZE = 1001;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] oznake = new int[n];
-        int lim = Math.max(n, k);
-        int[] freq = new int[1001];
+        int[] freq = new int[MAX_SIZE];
         long totalPairs = 0;
         for(int i = 0; i < n; i++) {
             int oznaka = sc.nextInt();
             freq[oznaka]++;
             oznake[i] = oznaka;
         }
-
         for(int i = 0; i < n; i++) {
             int target = k - oznake[i];
-            if(target < 0 || target > 1000) continue;
+            if(target < 0 || target >= MAX_SIZE) continue;
             totalPairs += freq[target];
         }
         System.out.println(totalPairs);
