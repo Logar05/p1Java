6ed6cc8029debdc656df7edd64db5e7f513f070a
Logar05 <logarsamuel1@gmail.com>
2025-08-16 04:54:35 +0200

    Izpit 2019/20, 1. rok

--- stariIzpiti/2019-20/izpit1/izhodisca/naloga1/Prva.java
+++ stariIzpiti/2019-20/izpit1/izhodisca/naloga1/Prva.java
@@ -10,6 +10,23 @@ import java.util.*;
 public class Prva {
 
     public static void main(String[] args) {
-        // dopolnite ...
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int k = sc.nextInt();
+        int lowerBound = 1;
+        int upperBound = a;
+        int count = 1;
+        int curPileSize = a;
+        while(true) {
+            if(lowerBound <= k && k <= upperBound){
+                System.out.println(count);
+                break;
+            }
+            count++;
+            curPileSize = curPileSize + b;
+            lowerBound = upperBound + 1;
+            upperBound = upperBound + curPileSize;
+        }
     }
 }
