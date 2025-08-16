80c89b9b278333d1932ed703617efefb88db662b
Logar05 <logarsamuel1@gmail.com>
2025-08-16 21:48:20 +0200

    Izpit 2019/20, drugi rok

--- stariIzpiti/2019-20/izpit2/izhodisca/naloga1/Prva.java
+++ stariIzpiti/2019-20/izpit2/izhodisca/naloga1/Prva.java
@@ -10,7 +10,27 @@ import java.util.*;
 public class Prva {
 
     public static void main(String[] args) {
-        // dopolnite ...
+        Scanner sc = new Scanner(System.in);
+        int count = 2;
+        boolean vsaDeljiva = true;
+        boolean skip = false;
+        int prvo = sc.nextInt();
+        int drugo = sc.nextInt();
+        if(drugo % prvo != 0) {
+            System.out.println(count - 1);
+            skip = true;
+            vsaDeljiva = false;
+        }
+        while (sc.hasNext() && !skip) {
+            count++;
+            prvo = drugo;
+            drugo = sc.nextInt();
+            if(drugo % prvo != 0) {
+                System.out.println(count - 1);
+                vsaDeljiva = false;
+            }
+        }
+        if(vsaDeljiva) System.out.println(count);
     }
 
     // po potrebi dopolnite ...
